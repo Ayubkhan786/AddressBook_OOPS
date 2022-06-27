4e4c2b6a1b455bf5dbc9ca9ca890bef130da5ec2
your Name <mohamedabdulayubm@gmail.com>
2022-06-27 13:50:12 +0530

    Creating Contacts

---
+++ AddressBook_OOPS/AddressBook.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_OOPS
+{
+    public class Contacts
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string city { get; set; }
+        public string State { get; set; }
+        public string email { get; set; }
+        public int Zip { get; set; }
+        public int phone { get; set; }
+
+    }
+    public class AddressBook
+    {
+        public void Contact()
+        {
+            List<Contacts> contactList = new List<Contacts>();
+            contactList.Add(new Contacts
+            {
+                Name = "Ayub",
+                Address = "India",
+                city = "Chennai",
+                State = "TN",
+                email = "Ayub@gmail.com",
+                Zip = 894515,
+                phone = 21161458
+            });
+
+            contactList.Add(new Contacts
+            {
+                Name = "Vijay",
+                Address = "India",
+                city = "Bihar",
+                State = "BH",
+                email = "Vijay@gmail.com",
+                Zip = 897815,
+                phone = 2115461458
+            });
+
+            contactList.Add(new Contacts
+            {
+                Name = "Nithish",
+                Address = "India",
+                city = "Bengaluru",
+                State = "KA",
+                email = "Nithish@gmail.com",
+                Zip = 8989955,
+                phone = 2116187458
+            });
+
+            contactList.Add(new Contacts
+            {
+                Name = "Sri",
+                Address = "India",
+                city = "Mumbai",
+                State = "MH",
+                email = "Sri@gmail.com",
+                Zip = 8956515,
+                phone = 21161568
+            });
+
+
+            foreach (var contacts in contactList)
+            {
+                Console.WriteLine(contacts.Name);
+                Console.WriteLine(contacts.Address);
+                Console.WriteLine(contacts.city);
+                Console.WriteLine(contacts.State);
+                Console.WriteLine(contacts.email);
+                Console.WriteLine(contacts.Zip);
+                Console.WriteLine(contacts.phone);
+                Console.WriteLine("*******************************************************************");
+            }
+        }
+    }
+}
+
+
--- AddressBook_OOPS/Program.cs
+++ AddressBook_OOPS/Program.cs
@@ -1,2 +1,55 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AddressBook_OOPS;
+
+
 Console.WriteLine("Welcome to Address Book Program");
+
+
+AddressBook addressBook = new AddressBook();
+addressBook.Contact();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// To create Contact
+
+
+//To Add Contact
+
+//contactPerson Person = new contactPerson();
+
+//    Person.Name = Console.ReadLine();
+//    Person.Address = Console.ReadLine();
+//    Person.city = Console.ReadLine();
+//    Person.State = Console.ReadLine();
+//    Person.email = Console.ReadLine();
+//    Person.Zip = Convert.ToInt32(Console.ReadLine());
+//    Person.phone = Convert.ToInt32(Console.ReadLine());
+//    Console.WriteLine(Person.Name);
+//    Console.WriteLine(Person.Address);
+//    Console.WriteLine(Person.city);
+//    Console.WriteLine(Person.State);
+//    Console.WriteLine(Person.email);
+//    Console.WriteLine(Person.Zip);
+//    Console.WriteLine(Person.phone);
+//    Console.WriteLine("*****************************------------------------------------------**************************************");
+
+
+
+
+
