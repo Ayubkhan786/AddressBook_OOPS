555b522dcade38fa3fb76006021eb7533633e713
your Name <mohamedabdulayubm@gmail.com>
2022-06-27 14:05:37 +0530

    Contacts

--- AddressBook_OOPS/Program.cs
+++ AddressBook_OOPS/Program.cs
@@ -26,30 +26,5 @@ addressBook.Contact();
 
 
 
-// To create Contact
-
-
-//To Add Contact
-
-//contactPerson Person = new contactPerson();
-
-//    Person.Name = Console.ReadLine();
-//    Person.Address = Console.ReadLine();
-//    Person.city = Console.ReadLine();
-//    Person.State = Console.ReadLine();
-//    Person.email = Console.ReadLine();
-//    Person.Zip = Convert.ToInt32(Console.ReadLine());
-//    Person.phone = Convert.ToInt32(Console.ReadLine());
-//    Console.WriteLine(Person.Name);
-//    Console.WriteLine(Person.Address);
-//    Console.WriteLine(Person.city);
-//    Console.WriteLine(Person.State);
-//    Console.WriteLine(Person.email);
-//    Console.WriteLine(Person.Zip);
-//    Console.WriteLine(Person.phone);
-//    Console.WriteLine("*****************************------------------------------------------**************************************");
-
-
-
 
 
